65c207cb44bccbf00aed7c5b0928915bac31c689
ryancheung <ryancheung.go@gmail.com>
2020-02-28 21:16:31 +0800

    Init.

---
+++ MonoGame.Shapes/Polyline.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+
+namespace MonoGame.Shapes
+{
+    public class Polyline
+    {
+        public Polyline(IEnumerable<Vector2> points)
+        {
+            Points = points;
+        }
+
+        public IEnumerable<Vector2> Points { get; private set; }
+        public float Left => Points.Min(p => p.X);
+        public float Top => Points.Min(p => p.Y);
+        public float Right => Points.Max(p => p.X);
+        public float Bottom => Points.Max(p => p.Y);
+
+        public Rectangle BoundingRectangle
+        {
+            get
+            {
+                var minX = (int)Left;
+                var minY = (int)Top;
+                var maxX = (int)Right;
+                var maxY = (int)Bottom;
+                return new Rectangle(minX, minY, maxX - minX, maxY - minY);
+            }
+        }
+
+        public bool Contains(float x, float y)
+        {
+            return false;
+        }
+
+        public bool Contains(Vector2 point)
+        {
+            return Contains(point.X, point.Y);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+## Shapes extensions for MonoGame
