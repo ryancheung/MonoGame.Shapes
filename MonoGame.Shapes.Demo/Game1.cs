f64d34097c9355c95c9112c97ae8121e4b011e2a
ryancheung <ryancheung.go@gmail.com>
2020-03-17 18:06:07 +0800

    update demo

--- MonoGame.Shapes.Demo/Game1.cs
+++ MonoGame.Shapes.Demo/Game1.cs
@@ -46,8 +46,24 @@ namespace MonoGame.Shapes.Demo
 
             _spriteBatch.Begin();
 
-            _spriteBatch.DrawLine(new Vector2(0, 0), new Vector2(0, 100), Color.Orange);
-            _spriteBatch.DrawLine(new Vector2(0, 0), new Vector2(100, 0), Color.Orange);
+            var pos1 = new Vector2(1, 0);
+            var pos2 = new Vector2(1+100, 0);
+            var pos3 = new Vector2(1+100, 0+100);
+            var pos4 = new Vector2(1, 0+100);
+
+            _spriteBatch.DrawLine(pos1, pos2, Color.Orange);
+            _spriteBatch.DrawLine(pos2, pos3, Color.Orange);
+            _spriteBatch.DrawLine(pos3, pos4, Color.Orange);
+            _spriteBatch.DrawLine(pos4, pos1, Color.Orange);
+
+            _spriteBatch.DrawRectangle(new Rectangle(1, 1+100, 100, 100), Color.Yellow);
+
+            _spriteBatch.FillRectangle(new Rectangle(1, 1*2+100*2, 100, 100), Color.Violet);
+
+            pos1 = new Vector2(200, 1);
+            pos2 = new Vector2(200, 1+100);
+            _spriteBatch.DrawLine(pos2, pos1, Color.Red);
+            _spriteBatch.DrawLine(pos1, pos2, Color.Orange);
 
             _spriteBatch.End();
 
